9ce43c84f80dcd0c8f23852bd8e68b5f84a1c9e6
Niglou <galaxneil.super@gmail.com>
2020-04-01 16:59:09 +0200

    update MAX7456 functions

--- main/drivers/osd/max7456.cpp
+++ main/drivers/osd/max7456.cpp
@@ -16,11 +16,11 @@ void MAX7456::spi_init() const {
   _spi->bit_order(MSB_FIRST, MSB_FIRST);
   _spi->byte_order(SPI_LITTE_ENDIAN, SPI_LITTE_ENDIAN);
   _spi->full_duplex(NORMAL);
-  _spi->phase(SPI_DIS, SPI_DIS, SPI_DIS, SPI_EN, SPI_DIS);
+  _spi->phase( SPI_ADDR | SPI_MOSI );
   _spi->addr_len(7);
   _spi->mosi_len(7);
   _spi->miso_len(7);
-  _spi->clk_sys(SPI_DIS);
+  _spi->clk_sys(0);
   _spi->pre_div(1);
   _spi->clkcnt(3);
   _spi->clk_edge_idle(LOW);
@@ -29,22 +29,14 @@ void MAX7456::spi_init() const {
   _spi->clear_all_buffer();
 }
 
+void MAX7456::turnOff() const {
+  WRegister(VM0,  0x01);
+}
+
 void MAX7456::init() const {
-  WRegister(W_VM0,  0x6C);
-  WRegister(W_OSDM, 0x00); // Insertion Mux
-  WRegister(W_VOS,  0b11111); // Vertical Offset
-
-
-  unsigned int addr = 30*2+15;
-  char addrH = addr >> 8;
-  char addrL = addr;
-
-  WRegister(W_DMM,  0x00);
-  WRegister(W_DMAH, addrH);
-  WRegister(W_DMAL, addrL);
-
-  WRegister(W_DMDI, 0x01);
-
+  WRegister(VM0,  0x6C);
+  WRegister(OSDM, 0x00); // Insertion Mux
+  WRegister(VOS,  0b11111); // Vertical Offset
 }
 
 int MAX7456::check() const {
@@ -52,55 +44,70 @@ int MAX7456::check() const {
 }
 
 char MAX7456::stat() const {
-  return RRegister(0xA0);
+  return RRegister(R_STAT); // Status register
 }
 
-void MAX7456::print(char *value, char x, char y) {
+void MAX7456::print(char x, char y, char *value, char length) {
   unsigned int addr = 30*y+x;
   char addrH = addr >> 8;
   char addrL = addr;
 
-  _spi->keep_cs(SPI_EN);
+  // 31 character max
+  length &= 0x1F;
 
-  WRegister(W_DMM, 0x01);
-  WRegister(W_DMAH, addrH);
-  WRegister(W_DMAL, addrL);
+  // Use CMD, ADDR and MOSI phases
+  _spi->phase( SPI_CMD | SPI_ADDR | SPI_MOSI );
 
-  for (int i = 0; i < 5; i++) {
-      WRegister(W_DMDI, value[i]);
+  // Use INCREMENT MODE
+  _spi->cmd_len(15);
+  _spi->cmd( (DMM << 8) | 0x01 );
+
+  // Set start ADDR DISPLAY
+  _spi->addr_len(31);
+  _spi->addr( (DMAH << 24) | (addrH << 16) | (DMAL << 8) | addrL );
+
+  // Number of bits
+  _spi->mosi_len( 16*length -1);
+
+  // If one character
+  if( length == 1 ) {
+    _spi->buffer(0, (DMDI << 8) | value[0]);
   }
-
-  WRegister(W_DMDI, 0xFF);
-
-  _spi->keep_cs(SPI_DIS);
-
-
-/*
-  for(int i=0; i<5; i++) {
-    WRegister(W_DMAL, addrL);
-    WRegister(W_DMDI, value[i]);
-    addrL++;
+  // If number of characters impair
+  else if( length%2 ) {
+    int len = length / 2;
+    for(int i=0; i<len; i++) {
+      int val = i * 2;
+      _spi->buffer(i, (DMDI << 24) | (value[val]<<16) | (DMDI << 8) | value[val+1]);
+    }
+    _spi->buffer(len, (DMDI << 24) | (value[length-1]<<16) | (DMDI << 8) | 0xFF);
   }
-*/
+  // If number of characters pair
+  else {
+    int len = length / 2;
+    for(int i=0; i<len; i++) {
+      int val = i * 2;
+      _spi->buffer(i, (DMDI << 24) | (value[val]<<16) | (DMDI << 8) | value[val+1]);
+    }
+    _spi->buffer(len, (DMDI << 8) | 0xFF);
+  }
 
 }
 
 void MAX7456::WRegister(unsigned char addr, unsigned char data) const {
-  _spi->phase(SPI_DIS, SPI_EN, SPI_DIS, SPI_EN, SPI_DIS);
+  _spi->phase( SPI_ADDR | SPI_MOSI );
   _spi->addr(addr << 24);
-  _spi->addr_len(7);
   _spi->buffer(0, data);
   _spi->mosi_len(7);
   _spi->transfer();
-  while(_spi->transfer_st());
+  while(_spi->transfer_status());
 }
 
 unsigned char MAX7456::RRegister(unsigned char addr) const {
-  _spi->phase(SPI_DIS, SPI_EN, SPI_DIS, SPI_DIS, SPI_EN);
-  _spi->addr(addr << 24);
-  _spi->addr_len(7);
+  _spi->phase( SPI_ADDR | SPI_MISO );
+  _spi->addr( ( 0x80 | addr ) << 24);
   _spi->miso_len(7);
   _spi->transfer();
-  while(_spi->transfer_st());
+  while(_spi->transfer_status());
   return (_spi->buffer(0));
 }
